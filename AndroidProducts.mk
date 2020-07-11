7384b6111c7dc89ec9e8421f1f82cd69413e957c
ripee <ripee.dev@gmail.com>
2020-07-11 11:20:53 +0200

    nobleltezt: Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nobleltezt.mk
+    $(LOCAL_DIR)/havoc_nobleltezt.mk
     
 COMMON_LUNCH_CHOICES := \
-    lineage_nobleltezt-user \
-    lineage_nobleltezt-userdebug \
-    lineage_nobleltezt-eng
+    havoc_nobleltezt-user \
+    havoc_nobleltezt-userdebug \
+    havoc_nobleltezt-eng
