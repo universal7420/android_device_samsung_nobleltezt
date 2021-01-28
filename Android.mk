0a7d558b8d99028a1d58c368cc0a4f0d1a5eed5c
ripee <ripee.dev@gmail.com>
2021-01-28 23:34:06 +0100

    nobleltezt: Add noblelte to assert

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 LOCAL_PATH := $(call my-dir)
 
 # Support installing on all noblelte targets regardless of stability or bootability
-ifneq ($(filter nobleltextc nobleltezt,$(TARGET_DEVICE)),)
+ifneq ($(filter noblelte nobleltextc nobleltezt,$(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
